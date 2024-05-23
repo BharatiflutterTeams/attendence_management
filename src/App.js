da62d12ac8042b24aa8cb03d5b56e726e6a48131
prasadparik-softtech <prasad.parik@bhartisofttech.com>
2024-05-23 14:34:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import BookingList from './pages/BookingList';
+import NotFound from './pages/PageNotFound';
+import Grid from '@mui/material/Grid';
+import Plans from './pages/Plans';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2} style={{height:"100vh"}}>
+  
+    <Grid item md={2} style={{background:"#2D3250"}}
+    >
+ <Sidebar />
+    </Grid>
+    <Grid item md={10} p={5}  style={{background:"#EEF5FF"}}>
+ 
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bookings" element={<BookingList />} />
+        <Route path="/plans" element={<Plans/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+
+    </Grid>
+    
+  </Grid>
+
+
+
+ 
   );
-}
+};
 
 export default App;
